62d96fca79fa75222104c9daa32c4dd379237dbd
Sohail <sohail.patel@neosoftmail.com>
2025-06-27 18:58:53 +0530

    Update provider.tf

--- Resource-Group/provider.tf
+++ Resource-Group/provider.tf
@@ -5,6 +5,13 @@ terraform {
       version = ">=4.34.0"
     }
   }
+
+  backend "azurerm" {
+    resource_group_name  = "Terraform-POC"
+    storage_account_name = "i2dstgacc"
+    container_name       = "terraform"
+    key                  = "tf/terraform.tfstate"
+  }
 }
 
 # Configure the Microsoft Azure Provider
